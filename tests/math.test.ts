6219f39dd3bf51ad941a02950cb68377928f3e06
seannian <seannians71@gmail.com>
2025-03-12 16:36:39 -0700

    tests

--- tests/math.test.ts
+++
@@ -1,5 +0,0 @@
-describe('Example Test Suite', () => {
-    it('Example Test - Should pass', () => {
-      expect(true).toBe(true); // A very basic test that always passes
-    });
-  });
---
+++ tests/unit/home.test.tsx
@@ -0,0 +1,13 @@
+// tests/unit/home.test.tsx
+
+import { render, screen } from '@testing-library/react';
+import Home from '@/app/page'; // Path to your Home component
+import '@testing-library/jest-dom';
+
+describe('HomePage Unit Test', () => {
+  it('renders "Hello world" text', () => {
+    render(<Home />);
+    const helloWorldElement = screen.getByText('Hello world');
+    expect(helloWorldElement).toBeInTheDocument();
+  });
+});
