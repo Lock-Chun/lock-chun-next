0bd12987102ffd2cc6fd3a4e877ef8c56c86e3c9
Josh <joshualomelin@gmail.com>
2025-04-23 13:06:48 -0700

    Removed unnecessary dark mode definitions

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -8,7 +8,7 @@ export default function NotFound() {
       <div className="flex-col flex items-center justify-center my-5 gap-5">
         <h2 className="text-6xl font-bold">404 - Not Found</h2>
         <p className="text-4xl">That page doesn&apos;t exist.</p>
-        <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-3xl px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+        <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-3xl px-5 py-2.5 me-2 mb-2">
           <Link href="/">Return Home</Link>
         </button>
       </div>
