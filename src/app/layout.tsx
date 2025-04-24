818d5204228f48f1ff7a908973b58982c06afe51
Josh <joshualomelin@gmail.com>
2025-04-23 21:30:48 -0700

    Metadata changes to define viewport in homepage styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <meta className="viewport" content="width=device-width, initial-scale=1.0"></meta>
       <body
         className={`${crimson.className} antialiased`}
       >
